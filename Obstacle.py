f8937c7aeb0fdb9ae7b24e17ec6efe5ce4baeddf
Chris Caromicoli <chriscaromicoli@Chriss-MacBook-Pro.local>
2018-11-13 00:11:13 -0500

    New Obstacle code

--- Obstacle.py
+++ Obstacle.py
@@ -1,68 +1,28 @@
 import math
+from shapely.geometry import Polygon
+from shapely.geometry import Point
+from shapely.geometry import LineString
+
 class obstacle(object):
 
     def __init__(self,points_):
-        self.points = list(points_)
-                
-    def checkInside(self,point):
-        #Append to self points the point 
-        copy=self.points
-        copy.append(point)
-        minYP = min(copy, key = lambda x:x[1])
-        def polarAngle(p):
-            px = p[0]-minYP[0]
-            py = p[1]-minYP[1]
-            return math.atan2(py,px)
-        sortedByPolar = sorted(copy, key = polarAngle)
-        lengthof=len(sortedByPolar)
-        flag=0
-        secondlast=sortedByPolar[lengthof-2]
-        last=sortedByPolar[lengthof-1]
-        firstcheck=sortedByPolar[0]
-        secondcheck=sortedByPolar[1]
-        if 0>=((last[0]-secondlast[0])*(firstcheck[1]-secondlast[1]))-((firstcheck[0]-secondlast[0])*(last[1]-secondlast[1])):
-            flag=1
-        if 0>=((firstcheck[0]-last[0])*(secondcheck[1]-last[1]))-((secondcheck[0]-last[0])*(firstcheck[1]-last[1])):
-            flag=1
-        for i in range(2,len(sortedByPolar)):
-            first=sortedByPolar[i-2]
-            second=sortedByPolar[i-1]
-            third=sortedByPolar[i]
-            val=((second[0]-first[0])*(third[1]-first[1]))-((third[0]-first[0])*(second[1]-first[1])) 
-            #Right Turn or colinear
-            if val<=0:
-                flag=1
-        if flag==1:
-            return True
-        else:
-            return False
+        self.points = Polygon(points_)
 
+    #Checks to see if a given point is within an obstacle including if it is on the boundary
+    #Returns true if the point is in the shape
+    #Returns false if the point is not in the shape            
+    def checkInside(self,point):
+        checkpoint=Point(point)
+        flag=self.points.intersects(checkpoint)
+        return flag
+
+    #Checks to see if a line is within an obstacle
+    #Returns true if the line passes through the shape
+    #Returns false if the line does not pass through the shape
     def checkPassThrough(self,point1,point2):
-        slopepoints=(point2[1]-point1[1])/(point2[0]-point1[0])
-        b1=point2[1]-slopepoints*point2[0]
-        for i in range (0,len(self.points)-1):
-            first=self.points[i+1]
-            second=self.points[i]
-            slopeself=(second[1]-first[1])/(second[0]-first[0])
-            b2=second[1]-slopeself*second[0]
-            xintersect=(b1-b2)/(slopepoints-slopeself)
-            if xintersect>=first[0] and xintersect<=second[0]:
-                return True
-        return False
+        line = LineString([point1,point2])
+        flag=self.points.intersects(line)
+        return flag
 
     def printPoints(self):
-        for elem in self.points:
-            print("("+str(elem[0])+", "+str(elem[1])+")")
-
-
-obj1 = obstacle([(0,0),(1,0),(1,1),(0,1)])
-
-obj1.printPoints()
-if obj1.checkInside((1.5,0.5))==True:
-    print('True')
-else:
-    print('False')
-if obj1.checkPassThrough((1.5,0.5),(0,0))==True:
-    print('True')
-else:
-    print('False')
+        print(self.points.bounds)
