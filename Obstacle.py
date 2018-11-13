cc9fdafdd08457370e7e74dc760c8d9f57fb17b8
Chris Caromicoli <chriscaromicoli@Chriss-MacBook-Pro.local>
2018-11-13 10:25:36 -0500

    New Obstacle code

--- Obstacle.py
+++ Obstacle.py
@@ -5,6 +5,7 @@ from shapely.geometry import LineString
 
 class obstacle(object):
 
+    #Initializes the polygon which represents the obstacle
     def __init__(self,points_):
         self.points = Polygon(points_)
 
@@ -16,7 +17,7 @@ class obstacle(object):
         flag=self.points.intersects(checkpoint)
         return flag
 
-    #Checks to see if a line is within an obstacle
+    #Checks to see if a line is within an obstacle, point 1 refers to current position and point 2 is where it wants to go
     #Returns true if the line passes through the shape
     #Returns false if the line does not pass through the shape
     def checkPassThrough(self,point1,point2):
@@ -24,5 +25,6 @@ class obstacle(object):
         flag=self.points.intersects(line)
         return flag
 
+    #Print the max bounds and min bounds for x and y in that order
     def printPoints(self):
         print(self.points.bounds)
